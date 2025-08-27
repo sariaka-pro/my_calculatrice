4cf26728a9c6244d605588d38ff157b3299acac1
sariaka.pro <sariakaschool@yahoo.com>
2025-08-27 15:48:26 +0200

    test changes

--- test_calculatrice.js
+++ test_calculatrice.js
@@ -12,56 +12,60 @@ let options = new chrome.Options();
 
 (async function testCalculatrice() {
 // Ici on crée directement le service
-let service = new chrome.ServiceBuilder("/usr/bin/chromedriver");
+//// let service = new chrome.ServiceBuilder("/usr/bin/chromedriver");
 
 let driver = await new Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
-    .setChromeService(service)   // ✅ au lieu de setDefaultService
+    ///.setChromeService(service)   // ✅ au lieu de setDefaultService
     .build();
 
     try {
         // Accéder au site
-        await driver.get('http://localhost:8080/index.html')
+        await driver.get('http://localhost:8080/index.html');
         // --- Test 1 : Vérifier l'Addition ---
         await driver.findElement(By.id('number1')).sendKeys('12'); 
         await driver.findElement(By.id('number2')).sendKeys('2'); 
-        await driver.findElement(By.css('option[value="add"]')).click; 
+        await driver.findElement(By.id('operation')).sendKeys('Addition');
         await driver.findElement(By.id('calculate')).click();
         // Afficher les résultats
-        let resultadd = await driver.findElement(By.id('result')).getText(resultadd);
-        console.log('Voici le résultat de l`addition : ' + resultadd);
+        let resultadd = await driver.findElement(By.id('result')).getText();
+        console.log(resultadd);
         await sleep(1000);
 
 
         // --- Test 2 : Division par Zéro ---
+        await driver.findElement(By.id('number1')).clear(); 
+        await driver.findElement(By.id('number2')).clear(); 
         await driver.findElement(By.id('number1')).sendKeys('20'); 
         await driver.findElement(By.id('number2')).sendKeys('0'); 
-        await driver.findElement(By.css('option[value="divide"]')).click; 
+        await driver.findElement(By.id('operation')).sendKeys('Division');
         await driver.findElement(By.id('calculate')).click();
         // Afficher les résultats
-        let resultzero = await driver.findElement(By.id('result')).getText(resultzero);
-        console.log('Voici le résultat de la division : ' + resultzero);
+        let resultzero = await driver.findElement(By.id('result')).getText();
+        console.log(resultzero);
         await sleep(1000);
 
         // --- Test 3 : Entrée Non Valide ---
         await driver.findElement(By.id('number1')).sendKeys('e'); 
         await driver.findElement(By.id('number2')).sendKeys('e'); 
-        await driver.findElement(By.css('option[value="subtract"]')).click; 
+        await driver.findElement(By.id('operation')).sendKeys('Division');
         await driver.findElement(By.id('calculate')).click();
         // Afficher les résultats
-        let erreur = await driver.findElement(By.id('result')).getText(erreur);
-        console.log('Requête non aboutie : ' + erreur);
+        let erreur = await driver.findElement(By.id('result')).getText();
+        console.log(erreur);
         await sleep(1000);
 
-        // --- Test 4 : Vérifier la Soustraction ---
-        await driver.findElement(By.id('number1')).sendKeys('20'); 
+        // --- Test 4 : Vérifier la Soustraction --- // NE PAS OUBLIER DE CLEAR !!!!! SINON CA S'AJOUTE AUTOMATIQUEMENT
+        await driver.findElement(By.id('number1')).clear(); 
+        await driver.findElement(By.id('number2')).clear(); 
+        await driver.findElement(By.id('number1')).sendKeys('10'); 
         await driver.findElement(By.id('number2')).sendKeys('8'); 
-        await driver.findElement(By.css('option[value="subtract"]')).click; 
+        await driver.findElement(By.id('operation')).sendKeys('Soustraction');
         await driver.findElement(By.id('calculate')).click();
         // Afficher les résultats
-        let resultsub = await driver.findElement(By.id('result')).getText(resultsub);
-        console.log('Voici le résultat de la soustraction : ' + resultsub);
+        let resultsub = await driver.findElement(By.id('result')).getText();
+        console.log(resultsub);
         await sleep(1000);
 
     } finally {
